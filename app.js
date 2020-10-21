dc3f9ac49ed94a813560d73ce1db3964c9f0c1b9
Mike Preis <mikepreis@users.noreply.github.com>
2020-10-21 09:33:06 -0700

    adding sangria.html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("App.js loaded");
 
-document.addEventListener("DOMContentLoaded", function(){
-    // Handler when the DOM is fully loaded
-    document.getElementById("myText").value = "Johnny Bravo";
-  });
+let btnAdd = document.querySelector('#inc');
+let input = document.querySelector('servingCount');
+
+btnAdd.addEventListener('click', () =>{
+    input.value = parseInt(input.value) + 1;
+});
